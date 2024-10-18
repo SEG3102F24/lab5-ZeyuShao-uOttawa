4d8c5a9b4eaa94cc4b032714428744356cb0f3d0
Zeyu <zeyu.shao@hotmail.com>
2024-10-18 14:26:03 -0400

    finished lab

--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -1,19 +1,27 @@
-import { Injectable } from '@angular/core';
-import {BehaviorSubject, Observable} from "rxjs";
-import {Employee} from "../model/employee";
+import { inject, Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from "rxjs";
+import { Employee } from "../model/employee";
+import { EmployeeDbService } from './firestore/employee-db.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
   employees$: BehaviorSubject<readonly Employee[]> = new BehaviorSubject<readonly Employee[]>([]);
+  private employeeDb: EmployeeDbService = inject(EmployeeDbService);
+
+  constructor() {
+    this.employeeDb.getEmployees().subscribe(employees => {
+      this.employees$.next(employees);
+    });
+  }
 
   get $(): Observable<readonly Employee[]> {
     return this.employees$;
   }
 
   addEmployee(employee: Employee) {
-    this.employees$.next([...this.employees$.getValue(), employee]);
+    this.employeeDb.createEmployee(employee);
     return true;
   }
 }
